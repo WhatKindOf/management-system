a8135d2025f5ea39bd5e71df3a3d95c9894d7559
WhatKindOf <50726981+WhatKindOf@users.noreply.github.com>
2019-06-17 15:49:53 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import Customer from "./components/Customer";
 
-function App() {
-  return <div className="back-ground" />;
+const persons = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "홍길동",
+    birthday: "940730",
+    gender: "남자",
+    job: "대학생"
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "김영희",
+    birthday: "940423",
+    gender: "여자",
+    job: "대학생"
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "이철수",
+    birthday: "900211",
+    gender: "남자",
+    job: "직장인"
+  }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {persons.map(p => {
+          return (
+            <Customer
+              key={p.id}
+              id={p.id}
+              image={p.image}
+              name={p.name}
+              birthday={p.birthday}
+              gender={p.gender}
+              job={p.job}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
